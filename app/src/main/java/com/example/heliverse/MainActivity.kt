d3bca1b8c751545c0cb597d81be022bf6ad32af3
Vibhuti Kapoor <vibhutikapoor22@gmail.com>
2024-04-06 00:10:54 +0530

    Heliverse

---
+++ app/src/main/java/com/example/heliverse/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.heliverse
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
